9121046956727a81d51aa80dc04f53ed3a61e8f9
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2022-08-04 16:15:06 +0200

    Update bashrc_install.bash

--- scripts/bashrc_install.bash
+++ scripts/bashrc_install.bash
@@ -2,13 +2,15 @@
 echo "" >> ~/.bashrc
 echo "" >> ~/.bashrc
 echo "# Virutalenvwrapper settings" >> ~/.bashrc
-echo "export WORKON_HOME=$HOME/.virtualenv" >> ~/.bashrc
-echo "VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bashrc
-echo ". /home/reachy/.local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
+echo "export PROJECT_HOME=/home/reachy/Devel" >> ~/.bashrc
+echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bashrc
+echo "source /home/reachy/.local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+
 echo "" >> ~/.bashrc
 
 echo "# Adding some useful aliases" >> ~/.bashrc
-echo "source ~/reachy_tips/scripts/custom_aliases" >> ~/.bashrc
+echo "source ~/reachy_tips/config/custom_aliases" >> ~/.bashrc
 echo "" >> ~/.bashrc
 
 echo "# ROS configuration" >> ~/.bashrc
